2508dc7774178fb2004584237990cb2be4e663d1
denys-u <denys.udovik@nure.ua>
2021-03-28 21:36:32 +0300

    added intComparer

---
+++ Modul3_dz1/Modul3_dz1/IntComparer.cs
@@ -0,0 +1,24 @@
+﻿namespace Modul3_dz1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class IntComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if (x < y)
+            {
+                return -1;
+            }
+
+            if (x > y)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
