578e8fab8a946d1e0b58181b09b9541a387f95d0
denys-u <denys.udovik@nure.ua>
2021-03-28 21:32:46 +0300

    created class mylist

---
+++ Modul3_dz1/Modul3_dz1/MyList.cs
@@ -0,0 +1,27 @@
+﻿namespace Modul3_dz1
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class MyList<T> : IReadOnlyCollection<T>
+    {
+        private const int DefaultCount = 4;
+        private const int RateFactor = 2;
+
+        private T[] _array;
+        private int _capacity;
+
+        public MyList(int capacity)
+        {
+            _capacity = capacity;
+            _array = new T[_capacity];
+        }
+
+        public MyList()
+            : this(DefaultCount)
+        {
+        }
+    }
+}
